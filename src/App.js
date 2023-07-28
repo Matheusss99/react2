4789e258abf61a7ff5549c3b0b6368577f722645
mate surmanidze <112821758+Matheusss99@users.noreply.github.com>
2023-07-28 08:53:22 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Parent from "./Parent";
+
+function App() {
+  return (
+    <div className="App">
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
+
